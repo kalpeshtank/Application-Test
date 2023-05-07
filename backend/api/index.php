ad305e0d6162c6cd55b2f8e827600967f58ad84c
kalpesh143 <k.p.tank.143@gmail.com>
2023-05-07 10:31:53 +0530

    chnage heders

--- backend/api/index.php
+++ backend/api/index.php
@@ -6,9 +6,8 @@ require "../src/OrderItems.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
 
 $baseController = new Src\BaseClass();
 $requestMethod = $_SERVER["REQUEST_METHOD"];
